b1cf16d846b48aaba1f00f62310212080ce38af6
Alex Kozin <al@el-machine.com>
2025-03-05 17:19:39 +0500

    Update License

--- Sources/Pipe/Pipable/ARKit/ARAppClipCodeAnchor_Pipe.swift
+++ Sources/Pipe/Pipable/ARKit/ARAppClipCodeAnchor_Pipe.swift
@@ -1,11 +1,23 @@
-//
-//  ARAppClipCodeAnchor_Pipe.swift
-//  Pipe
-//
-//  Created by Alex Kozin on 20.10.2022.
-//
+///
+/// Copyright 2020 Alexander Kozin
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+/// Created by Alex Kozin
+/// El Machine 🤖
 
-import ARKit.ARAnchor
+#if canImport(ARKit)
+import ARKit
 
 @available(iOS 13.0, *)
 extension ARAnchor: Asking {
@@ -59,8 +71,6 @@ public extension Ask where T == Array<ARAnchor> {
 
 }
 
-import ARKit.ARAppClipCodeAnchor
-
 @available(iOS 14.3, *)
 @discardableResult
 public
@@ -73,3 +83,5 @@ prefix func | (ask: Ask<[ARAppClipCodeAnchor]>) -> Pipe {
     //Ask for [ARAnchor] with AR config
     return (configuration as ARConfiguration) | ask
 }
+
+#endif
